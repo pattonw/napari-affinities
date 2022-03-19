af158ef3f2a91421a9d20dc2fdd6d3d98f0e1eea
pattonw <pattonw@hhmi.org>
2022-03-18 20:00:51 -0700

    remove old train widget

--- src/napari_affinities/__init__.py
+++ src/napari_affinities/__init__.py
@@ -5,7 +5,6 @@ from ._reader import napari_get_reader
 from ._writer import write_single_image, write_multiple
 
 from .widget import (
-    train_affinities_widget,
     predict_affinities_widget,
     mutex_watershed_widget,
 )
