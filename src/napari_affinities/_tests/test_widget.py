c5764b4976d73337fce0aaf3c3b04cd4a5a5c086
Steffen Wolf <steffen.wolf.1989@gmail.com>
2022-04-06 16:51:47 +0100

    include mutex watershed widget test

--- src/napari_affinities/_tests/test_widget.py
+++ src/napari_affinities/_tests/test_widget.py
@@ -1,33 +1,25 @@
 from napari_affinities.widgets.affinities import (
     ModelWidget
 )
+from napari_affinities.widgets.mutex_watershed import (
+    mutex_watershed_widget
+)
 import numpy as np
 
 
 def test_train_affinities_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
-    raw = viewer.add_image(np.random.random((100, 100)))
-    gt = viewer.add_labels(np.random.random((100, 100), dtype=np.int32))
-    mask = viewer.add_labels(np.random.random((100, 100), dtype=np.int32))
-    lsds = viewer.add_image(np.random.random((100, 100)))
     model_widget = ModelWidget(viewer)
     my_widget = model_widget.create_train_widget(viewer)
-    my_widget(raw, gt, mask, lsds)
 
+def test_train_affinities_widget(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
+    model_widget = ModelWidget(viewer)
+    my_widget = model_widget.create_predict_widget(viewer)
 
-# def test_predict_affinities_widget(make_napari_viewer, capsys):
-#     viewer = make_napari_viewer()
-#     layer = viewer.add_image(np.random.random((100, 100)))
-
-#     my_widget = predict_affinities_widget()
-
-#     my_widget(viewer.layers[0])
-
-
-# def test_mutex_watershed_widget(make_napari_viewer, capsys):
-#     viewer = make_napari_viewer()
-#     layer = viewer.add_image(np.random.random((100, 100)))
-
-#     my_widget = mutex_watershed_widget()
-
-#     my_widget(viewer.layers[0])
+def test_mutex_watershed_widget(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
+    offsets = [[1, 0, 0]] * 5
+    layer = viewer.add_image(np.random.random((5, 100, 100)), metadata={"offsets": offsets})
+    my_widget = mutex_watershed_widget()
+    my_widget(viewer.layers[0])
