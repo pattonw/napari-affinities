f18045d5d53ce21461010950be3097e0f154b29c
pattonw <pattonw@hhmi.org>
2022-07-04 09:54:22 -0700

    pin specific versions for installation

--- setup.py
+++ setup.py
@@ -2,4 +2,13 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup()
+setup(install_requires=[
+    "lsd @ git+https://github.com/pattonw/lsd.git@no-convenience-imports",
+    "numpy",
+    "zarr",
+    "magicgui",
+    "bioimageio.core",
+    "gunpowder @ git+https://github.com/funkey/gunpowder.git@patch-1.2.3",
+    "affogato",
+    "matplotlib",
+])
