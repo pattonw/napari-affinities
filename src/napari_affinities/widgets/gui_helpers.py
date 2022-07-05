b5bc77680b4b1102f1d72d68a6ea1b1514ebaba5
pattonw <pattonw@hhmi.org>
2022-07-04 18:48:02 -0700

    add layer remove callback for resetting layer dropdowns

--- src/napari_affinities/widgets/gui_helpers.py
+++ src/napari_affinities/widgets/gui_helpers.py
@@ -37,6 +37,6 @@ class MainWindow(QtWidgets.QMainWindow):
 
 def layer_choice_widget(viewer, annotation, **kwargs) -> FunctionGui:
     widget = create_widget(annotation=annotation, **kwargs)
-    widget.reset_choices()
     viewer.layers.events.inserted.connect(widget.reset_choices)
+    viewer.layers.events.removed.connect(widget.reset_choices)
     return widget
