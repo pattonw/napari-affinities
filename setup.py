e2bc15da48ea2bf508fbc103e6def6f64555ee86
pattonw <pattonw@hhmi.org>
2022-09-09 11:29:19 -0700

    directly embed lsd code since lsd isn't yet published on pypi or conda

--- setup.py
+++ setup.py
@@ -2,13 +2,4 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(install_requires=[
-    "lsd @ git+https://github.com/pattonw/lsd.git@no-convenience-imports",
-    "numpy",
-    "zarr",
-    "magicgui",
-    "bioimageio.core",
-    "gunpowder @ git+https://github.com/funkey/gunpowder.git@patch-1.2.3",
-    "affogato",
-    "matplotlib",
-])
+setup()
