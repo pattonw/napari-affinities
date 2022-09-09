26ecf886f064e80426f3b1fb18b7230720c44748
pattonw <pattonw@hhmi.org>
2022-09-09 13:27:49 -0700

    re-add tests

--- src/napari_affinities/_tests/test_widget.py
+++ src/napari_affinities/_tests/test_widget.py
@@ -2,15 +2,15 @@
 # from napari_affinities.widgets.mutex_watershed import mutex_watershed_widget
 
 
-# def test_train_affinities_widget(make_napari_viewer, capsys):
-    # pass
+def test_train_affinities_widget(make_napari_viewer, capsys):
+    pass
     # viewer = make_napari_viewer()
     # model_widget = ModelWidget(viewer)
     # my_widget = model_widget.create_train_widget(viewer)
 
 
-# def test_mutex_watershed_widget(make_napari_viewer, capsys):
-    # pass
+def test_mutex_watershed_widget(make_napari_viewer, capsys):
+    pass
     # viewer = make_napari_viewer()
     # offsets = [[1, 0, 0]] * 5
     # layer = viewer.add_image(np.random.random((5, 100, 100)), metadata={"offsets": offsets})
